aa918a3a91b2c170dc5b7a602c6920279b48b171
zzoyu <zzoyu@hotmail.com>
2023-08-04 02:19:16 +0900

    feat: :sparkles: add font loading routine

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue";
-import BaseSection from "@/components/BaseSection.vue";
-import TheNavigation from "@/components/TheNavigation.vue";
-import TheRendererContainer from "@/components/TheRendererContainer.vue";
-import TheLightToggle from "@/components/TheLightToggle.vue";
-
-import Main from "@/components/sections/Main.vue";
-import About from "@/components/sections/About.vue";
-
-const sectionInfo = [
-  {
-    name: "Main",
-    component: Main,
-  },
-  {
-    name: "About",
-    component: About,
-  },
-];
-
-const sections = ref<InstanceType<typeof BaseSection>[] | null>(null);
-const currentIndex = ref(0);
-
-const moveToNextSection = () => {
-  if (sections.value === null || currentIndex.value === sectionInfo.length - 1)
-    return;
-  sections.value?.[currentIndex.value + 1].focus();
-};
-
-const isDarkMode = ref(
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-);
-
-watchEffect(() => {
-  if (isDarkMode.value) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-});
+import View from "@/View.vue";
 </script>
 
 <template>
-  <TheRendererContainer />
-  <TheLightToggle v-model="isDarkMode" />
-  <TheNavigation
-    class="fixed bottom-10 left-10"
-    :menuLabels="sectionInfo.map((section) => section.name)"
-    :currentIndex="currentIndex"
-  />
-  <div class="fixed right-10 bottom-10 flex flex-col justify-end items-center">
-    <button class="text-2xl animate-bounce" @click="moveToNextSection()">
-      ↓
-    </button>
-  </div>
-
-  <BaseSection
-    v-for="(section, index) in sectionInfo"
-    :key="index"
-    @enter="currentIndex = index"
-    ref="sections"
-  >
-    <component :is="section.component" />
-  </BaseSection>
+  <Suspense>
+    <template #fallback>
+      <div class="flex justify-center items-center w-full h-full grow">
+        <div class="text-4xl animate-pulse">Loading...</div>
+      </div>
+    </template>
+    <View />
+  </Suspense>
 </template>
 
 <style scoped>
---
+++ src/View.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { onMounted, ref, watchEffect } from "vue";
+import BaseSection from "@/components/BaseSection.vue";
+import TheNavigation from "@/components/TheNavigation.vue";
+import TheRendererContainer from "@/components/TheRendererContainer.vue";
+import TheLightToggle from "@/components/TheLightToggle.vue";
+
+import Main from "@/components/sections/Main.vue";
+import About from "@/components/sections/About.vue";
+
+const sectionInfo = [
+  {
+    name: "Main",
+    component: Main,
+  },
+  {
+    name: "About",
+    component: About,
+  },
+];
+
+const sections = ref<InstanceType<typeof BaseSection>[] | null>(null);
+const currentIndex = ref(0);
+
+const moveToNextSection = () => {
+  if (sections.value === null || currentIndex.value === sectionInfo.length - 1)
+    return;
+  sections.value?.[currentIndex.value + 1].focus();
+};
+
+const isDarkMode = ref(
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+);
+
+watchEffect(() => {
+  if (isDarkMode.value) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+});
+
+await document.fonts.ready;
+// add a delay for 1s to show the loading animation
+await new Promise((resolve) => setTimeout(resolve, 10000));
+</script>
+
+<template>
+  <TheRendererContainer />
+  <TheLightToggle v-model="isDarkMode" />
+  <TheNavigation
+    class="fixed bottom-10 left-10"
+    :menuLabels="sectionInfo.map((section) => section.name)"
+    :currentIndex="currentIndex"
+  />
+  <div class="fixed right-10 bottom-10 flex flex-col justify-end items-center">
+    <button class="text-2xl animate-bounce" @click="moveToNextSection()">
+      ↓
+    </button>
+  </div>
+
+  <BaseSection
+    v-for="(section, index) in sectionInfo"
+    :key="index"
+    @enter="currentIndex = index"
+    ref="sections"
+  >
+    <component :is="section.component" />
+  </BaseSection>
+</template>
+
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: black;
+}
+</style>
