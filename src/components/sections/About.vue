35c65908bac8203a10864ed44bb53c25417bb358
zzoyu <zzoyu@hotmail.com>
2026-02-23 05:57:21 +0900

    chore: :speech_balloon: update about section

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -20,17 +20,18 @@
     </ul>
     <ul>
       <li>
-        저는 유저와 직접 상호작용하는 모든 분야를 좋아합니다. 취미로는 게임
-        개발을 배우고 있습니다.
+        저는 유저와 직접 상호작용하는 모든 분야를 좋아합니다. 프론트엔드에
+        국한하지 않고, 이것저것 시도해 보는 것을 좋아합니다.
       </li>
 
       <li>
-        업무에서 Vue.js 3, Nuxt.js 3 위주의 프로젝트를 다수 수행하였습니다.
+        업무에서 Vue.js 3, Nuxt.js 3 위주의 프로젝트를 다수 수행하였으며,
+        React.js, Next.js 프로젝트도 경험이 있습니다.
       </li>
       <li>언어는 TypeScript, JavaScript를 주로 사용하고 있습니다.</li>
       <li>
-        현재는 이커머스 플랫폼 회사의 쇼핑몰 솔루션 개발팀에서 신규 프로젝트 및
-        유지보수 작업을 맡고 있습니다.
+        현재는 이커머스 플랫폼 회사의 데이터 서비스 팀에서 풀스택 개발자로
+        일하고 있습니다.
       </li>
     </ul>
 
@@ -38,12 +39,10 @@
       <li>
         <h3>FrontEnd</h3>
         <ul class="stack">
-          <li>Vue.js <mark>best</mark></li>
-          <li>Nuxt.js <mark>best</mark></li>
           <li>React.js</li>
           <li>Next.js</li>
-					<li>Flutter</li>
-          <li>Android(?)</li>
+          <li>Vue.js</li>
+          <li>Nuxt.js</li>
         </ul>
       </li>
 
@@ -51,7 +50,6 @@
         <h3>BackEnd</h3>
         <ul class="stack">
           <li>Express.js</li>
-          <li>PHP</li>
         </ul>
       </li>
 
@@ -61,10 +59,6 @@
           <li>JavaScript <mark>best</mark></li>
           <li>TypeScript <mark>best</mark></li>
           <li>Python</li>
-          <li>Kotlin</li>
-          <li>PHP</li>
-          <li>C/C++</li>
-					<li>Dart</li>
         </ul>
       </li>
 
@@ -72,8 +66,8 @@
         <h3>Etc.</h3>
         <ul class="stack">
           <li>Git</li>
-          <li>GitHub(+Action)</li>
-          <li>GitLab(+Workflow)</li>
+          <li>GitHub Action</li>
+          <li>GitLab Workflow</li>
           <li>Linux</li>
           <li>Docker</li>
           <li>Storybook</li>
@@ -85,9 +79,8 @@
         <h3>그 외 관심사🧐</h3>
         <ul class="stack">
           <li>Three.js</li>
-          <li>Phaser.js</li>
           <li>Arduino</li>
-          <li>Raspberry Pi</li>
+          <li>And more...</li>
         </ul>
       </li>
     </ul>
